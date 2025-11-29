be9e4cbc1cd6251af636c18b31f9a00b2b5055f8
Nobody12here <uraja01313@gmail.com>
2025-11-29 22:36:23 +0500

    refactor: update task management logic and enhance JSON handling

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,44 @@
 use core::panic;
-use serde_json::json;
+use serde::{Deserialize, Serialize};
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
 
-fn open_file(file_path: &str) {
+#[derive(Serialize, Deserialize)]
+struct Task {
+    id: u32,
+    task: String,
+    description: String,
+}
+fn store_json(file_path: &str, data: &str) {
     let path = Path::new(file_path);
-    let dispaly = path.display();
-    let mut file = match File::open(path) {
-        Err(error) => panic!("Error occured while loading file {} :: {}", dispaly, error),
+    let display = path.display();
+    let mut file = match File::create(path) {
+        Err(error) => panic!("Error occured while creating file {} :: {}", display, error),
         Ok(file) => file,
     };
-    let mut data_buff = String::new();
-    match file.read_to_string(&mut data_buff) {
-        Err(error) => panic!("Something went wrong while reading file {}", error),
-        Ok(length) => println!("{} contains \n{}\n length {}", dispaly, data_buff, length),
+
+    match file.write_all(data.as_bytes()) {
+        Err(error) => panic!("Something went wrong while writing file {}", error),
+        Ok(_) => println!("Sucessfully written to {}", display),
     }
 }
 
-fn add_task(task: &str) {
-    let json_value = json!({
-        "name":"test",
-        "age":10,
-        "phones":[
-            "+92 303 5622496",
-            "+92 316 5622496"
-        ]
-    });
-    open_file("./task.json");
+fn add_task(task: &str, description: &str) {
+    let task_value = Task {
+        id: 1,
+        task: task.to_owned(),
+        description: description.to_owned(),
+    };
+    let json_string = match serde_json::to_string(&task_value) {
+        Ok(result) => result,
+        Err(error) => panic!(
+            "Something went wron while parsing struct to string {}",
+            error
+        ),
+    };
+    store_json("./task.json", &json_string);
 }
 
 fn main() {
@@ -38,7 +48,8 @@ fn main() {
     match operation {
         "add" => {
             let task = args.get(2).expect("Task not provided");
-            add_task(task) // the deref coercsion will auto convert &String to &str
+            let description: &String = args.get(3).expect("description not provided");
+            add_task(task, description) // the deref coercsion will auto convert &String to &str
         }
         "update" => println!("Update"),
         "delete" => println!("delete"),
